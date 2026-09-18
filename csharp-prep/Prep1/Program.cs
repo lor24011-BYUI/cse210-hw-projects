6af6164d07137f46764f40c4601fbe0e7336f762
lor24011-BYUI <lor2401@byui.edu>
2026-09-18 10:51:21 -0600

    Added strings and format

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,18 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        // Initialize strings
+        string firstName;
+        string lastName;
+
+        // Print to terminal and recieve string from user
+        Console.WriteLine("Please enter your first name: ");
+        firstName = Console.ReadLine();
+        Console.WriteLine("Please enter your last name: ");
+        lastName = Console.ReadLine();
+
+        // Format string and print to terminal
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
