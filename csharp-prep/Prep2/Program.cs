da61ac612ff048b10ef8514c16b7207bec4a3159
lor24011-BYUI <lor2401@byui.edu>
2026-09-21 10:14:42 -0600

    Added if statments for letter grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,6 +8,11 @@ class Program
         Console.WriteLine("What is your Grade Percentage: ");
         string userInput = Console.ReadLine();
         int gradePercentage = int.Parse(userInput);
+    
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Your letter grade is A");
+        }
 
     }
 }
