588d5c557ae276e186a1e93d112ec1d4c1bbfeb7
lor24011-BYUI <lor2401@byui.edu>
2026-09-23 10:54:37 -0600

    Asked user for number and guess then hinted to user

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,36 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        // Title
+        Console.WriteLine("Welcome to Guess My Number!");
+
+        // Ask User for Magic Number
+        Console.Write("What is the magic number? ");
+        string magicNumberString = Console.ReadLine();
+        int magicNumber = int.Parse(magicNumberString);
+
+        // Ask User for guess
+        Console.Write("What is your guess? ");
+        string magicGuessString = Console.ReadLine();
+        int magicGuess = int.Parse(magicGuessString);
+
+        // Less then magicNumber
+        if (magicGuess < magicNumber)
+        {
+            Console.WriteLine("Higher ");
+        }
+
+        // Greater than magicNumber
+        if (magicGuess > magicNumber)
+        {
+            Console.WriteLine("Lower ");
+        }
+
+        // magicGuess == magicNumber
+        if (magicGuess == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
