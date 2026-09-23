3fc32998b73f9e16ebe0bd9e4723212a31eb6e90
lor24011-BYUI <lor2401@byui.edu>
2026-09-23 11:05:41 -0600

    User loops with a do while loop and exits when entering a 0

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,34 +5,48 @@ class Program
     static void Main(string[] args)
     {   
         // Title
-        Console.WriteLine("Welcome to Guess My Number!");
+        Console.WriteLine("Welcome to Guess My Number! \nEnter 0 to exit");
 
         // Ask User for Magic Number
         Console.Write("What is the magic number? ");
         string magicNumberString = Console.ReadLine();
         int magicNumber = int.Parse(magicNumberString);
 
-        // Ask User for guess
-        Console.Write("What is your guess? ");
-        string magicGuessString = Console.ReadLine();
-        int magicGuess = int.Parse(magicGuessString);
-
-        // Less then magicNumber
-        if (magicGuess < magicNumber)
+        // Initialize magicGuess
+        int magicGuess = 0;
+        // Do-While loop (magicGuess != 0)
+        do 
         {
-            Console.WriteLine("Higher ");
-        }
 
-        // Greater than magicNumber
-        if (magicGuess > magicNumber)
-        {
-            Console.WriteLine("Lower ");
-        }
+            // Ask User for guess
+            Console.Write("What is your guess? ");
+            string magicGuessString = Console.ReadLine();
+            magicGuess = int.Parse(magicGuessString);
 
-        // magicGuess == magicNumber
-        if (magicGuess == magicNumber)
-        {
-            Console.WriteLine("You guessed it!");
-        }
+            // Exit Number
+            if (magicGuess == 0)
+            {
+                Console.WriteLine();
+            }
+
+            // Less then magicNumber
+            else if (magicGuess < magicNumber)
+            {
+                Console.WriteLine("Higher ");
+            }
+
+            // Greater than magicNumber
+            else if (magicGuess > magicNumber)
+            {
+                Console.WriteLine("Lower ");
+            }
+
+            // magicGuess == magicNumber
+            else if (magicGuess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (magicGuess != 0);
     }
 }
