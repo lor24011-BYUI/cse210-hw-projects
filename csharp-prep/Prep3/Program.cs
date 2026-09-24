9d7c4638a254db1fef072670bd446245c471ae31
lor24011-BYUI <lor2401@byui.edu>
2026-09-24 09:15:38 -0600

    Completed stretch challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,48 +5,74 @@ class Program
     static void Main(string[] args)
     {   
         // Title
-        Console.WriteLine("Welcome to Guess My Number! \nEnter 0 to exit");
+        Console.WriteLine("Welcome to Guess My Number!");
 
-        // Ask User for Magic Number
+        /* Ask User for Magic Number
         Console.Write("What is the magic number? ");
         string magicNumberString = Console.ReadLine();
         int magicNumber = int.Parse(magicNumberString);
+        */
+
+        // Produce a random magicNumber from 1 - 100
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,11);
+
+        // Initialize User Continue
+        string userContinue = "yes";
 
         // Initialize magicGuess
         int magicGuess = 0;
-        // Do-While loop (magicGuess != 0)
+
+        // Initialize
+        int guessTotal = 0;
+
+        // Do-While loop (userContinue == yes)
         do 
         {
-
-            // Ask User for guess
-            Console.Write("What is your guess? ");
-            string magicGuessString = Console.ReadLine();
-            magicGuess = int.Parse(magicGuessString);
-
-            // Exit Number
-            if (magicGuess == 0)
+            // Do-While loop (magicGuess != magicNumber)
+            do 
             {
-                Console.WriteLine();
-            }
 
-            // Less then magicNumber
-            else if (magicGuess < magicNumber)
-            {
-                Console.WriteLine("Higher ");
-            }
+                // Ask User for guess
+                Console.Write("What is your guess? ");
+                string magicGuessString = Console.ReadLine();
+                magicGuess = int.Parse(magicGuessString);
+                guessTotal++ ;
 
-            // Greater than magicNumber
-            else if (magicGuess > magicNumber)
-            {
-                Console.WriteLine("Lower ");
-            }
+                /* Exit Number
+                if (magicGuess == 0)
+                {
+                    Console.WriteLine();
+                }
+                */
 
-            // magicGuess == magicNumber
-            else if (magicGuess == magicNumber)
-            {
-                Console.WriteLine("You guessed it!");
-            }
+                // Less then magicNumber
+                if (magicGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher ");
+                }
 
-        } while (magicGuess != 0);
+                // Greater than magicNumber
+                else if (magicGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower ");
+                }
+
+                // magicGuess == magicNumber
+                else if (magicGuess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took {guessTotal} guesses");
+                    Console.Write("Would you like to continue? yes/no : ");
+                    userContinue = Console.ReadLine();
+
+                    if (userContinue == "yes")
+                    {
+                        guessTotal = 0;
+                    }
+                }
+
+            } while (magicGuess != magicNumber);
+        } while (userContinue == "yes");
     }
 }
